61342680a910515812e8bccd2c4a606961185e48
Ray Kwan <kwanlokto@gmail.com>
2018-11-03 10:52:37 -0400

    create a somewhat working scraper

--- news_scraper/news_scraper/items.py
+++ news_scraper/news_scraper/items.py
@@ -11,4 +11,7 @@ import scrapy
 class NewsScraperItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    title
+    url_from = scrapy.Field()
+    # The destination URL
+    url_to = scrapy.Field()
